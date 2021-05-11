252a7ce47ccd0a243c36c4c94e9628586bb9b53c
Yahor Vikharau <egor.vikharau@gmail.com>
2021-05-11 13:59:39 +0300

    * add TypeScript support for WebPack

--- src/wwwroot/webpack.config.js
+++ src/wwwroot/webpack.config.js
@@ -2,24 +2,22 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/Test.jsx',
+    entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public/js'),
     },
     devtool: 'inline-source-map', // enables react debug (i.e. source mapping for react bundled code)
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     module: {
         rules: [
           {
-            test: /\.m?jsx$/,
+            test: /\.tsx?$/,
             exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env', "@babel/preset-react"]
-              }
-            }
+            use: 'ts-loader',
           }
         ]
-      }
+    }
 }
