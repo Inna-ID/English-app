1db86f138c733f84d3ecba85f8133cb8c1ed1512
Inna-ID <gmccsilv@gmail.com>
2021-05-11 13:30:22 +0300

    create ts component

---
+++ src/wwwroot/src/components/App.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const App: React.FunctionComponent = () => {
+	return (
+		<div className="commentBox">
+         Hello, world from React! I am a CommentBox.
+      </div>
+	);
+}
+
+export default App;
