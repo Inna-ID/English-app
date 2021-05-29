3cd3728ff897265ad5510a6cebb1c0930f7fc16a
Inna-ID <gmccsilv@gmail.com>
2021-05-29 14:52:05 +0300

    change initial files

--- src/EnglishApp.Web/wwwroot/src/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-var App = function () {
-    return (React.createElement("div", { className: "commentBox" }, "Hello, world from React! I am a CommentBox."));
-};
-export default App;
--- src/EnglishApp.Web/wwwroot/src/components/App.tsx
+++ src/EnglishApp.Web/wwwroot/src/components/App.tsx
@@ -1,11 +1,11 @@
 ﻿import React from 'react';
 
 const App: React.FunctionComponent = () => {
-	return (
-		<div className="commentBox">
-         Hello, world from React! I am a CommentBox.
-      </div>
-	);
+    return (
+        <div className="commentBox">
+            Hello, world from React! I am a CommentBox.
+        </div>
+    );
 }
 
 export default App;
