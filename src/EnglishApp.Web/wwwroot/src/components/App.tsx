35e589f2d2d14bc3dadc976e918023e3bfea79f9
Inna-ID <gmccsilv@gmail.com>
2021-05-29 16:47:18 +0300

    add material ui, test button

--- src/EnglishApp.Web/wwwroot/src/components/App.tsx
+++ src/EnglishApp.Web/wwwroot/src/components/App.tsx
@@ -1,9 +1,12 @@
 ﻿import React from 'react';
+import LoginForm from './LoginForm/LoginForm';
 
+// TODO: add routes
 const App: React.FunctionComponent = () => {
     return (
         <div className="commentBox">
             Hello, world from React! I am a CommentBox.
+            <LoginForm/>
         </div>
     );
 }
---
+++ src/EnglishApp.Web/wwwroot/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const LoginForm = () => {
+    return (
+        <>
+            <Button variant="contained" color="primary">
+                Log in
+            </Button>
+        </>
+    )
+}
+
+export default LoginForm;
